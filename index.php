4e6ed4433d64d5f0fb4abdb759caeeb10534debb
Ulises Martinez <ulisesromanmartinez101098@gmail.com>
2020-07-17 19:23:32 -0500

    Cambie la etiqueta title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-    <title>Sistema de Votaciones</title>
+    <title>Sistema de Votaciones de la Universidad</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
